e38f1b056e26c835033a0b78484c71837958cc24
SyedAbdulRauf <110671589+SyedAbdulRauf@users.noreply.github.com>
2022-08-05 16:50:48 +0500

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,14 @@
+number1=int(input("enter the 1st number "))
+number2=int(input("enter the 2nd number "))
+
+
+sum=number1+number2
+difference=number1-number2
+product=number1*number2
+division=number1/number2
+
+print("sum:",sum)
+print("difference of numbers is ",difference)
+print("product",product)
+print("division",division)
+
